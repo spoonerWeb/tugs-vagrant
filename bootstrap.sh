1e228c5e8ace0702999f9e65910086f52b97434f
Thomas Löffler <loeffler@spooner-web.de>
2014-07-17 14:35:35 +0200

    Removes link to public virtual folder

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,9 +8,6 @@ sudo apt-get -y install mysql-server-5.5 php5-mysql apache2 php5 php5-gd graphic
 if [ ! -h /var/www ]; 
 then 
 
-    rm -rf /var/www sudo 
-    ln -s /vagrant/public /var/www
-
     a2enmod rewrite
 
     sed -i '/AllowOverride None/c AllowOverride All' /etc/apache2/sites-available/default
