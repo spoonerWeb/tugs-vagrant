f3600d8aafb2981c3d6b513683b6bc7a7b528989
Thomas Löffler <loeffler@spooner-web.de>
2014-07-17 11:19:06 +0200

    [TASK] Fixes some points
    * Uses private network with IP "192.168.150.5"
    * Configures the shared folder
    * Sets the mysql root password with "rootpass"

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,31 @@
 #!/usr/bin/env bash
 
-sudo -s
-apt-get update
-apt-get install -y apache2 php5 mysql-server wget
+sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password password rootpass'
+sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password_again password rootpass'
+sudo apt-get update
+sudo apt-get -y install mysql-server-5.5 php5-mysql apache2 php5 php5-gd graphicsmagick wget
+
+if [ ! -h /var/www ]; 
+then 
+
+    rm -rf /var/www sudo 
+    ln -s /vagrant/public /var/www
+
+    a2enmod rewrite
+
+    sed -i '/AllowOverride None/c AllowOverride All' /etc/apache2/sites-available/default
+
+    sudo sed -i.bak s/2M/10M/g /etc/php5/apache2/php.ini
+    sudo sed -i.bak s/max_execution_time = 30/max_execution_time = 240/g /etc/php5/apache2/php.ini
+    sudo sed -i.bak s/8M/10M/g /etc/php5/apache2/php.ini
+
+    service apache2 restart
+fi
+
 cd /var/www/
-mkdir typo3
-cd typo3
-wget get.typo3.org/6.2 -O typo3_src-latest.tar.gz
+wget -q get.typo3.org/6.2 -O typo3_src-latest.tar.gz
 tar xzf typo3_src-latest.tar.gz
+rm typo3_src-latest.tar.gz
 mv typo3_src-6.2.4 typo3_src
 ln -s typo3_src/typo3
 ln -s typo3_src/index.php
