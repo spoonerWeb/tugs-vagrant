f65c2a73e70dbdcdd3e414fdebe71e1ae771923e
Thomas Löffler <loeffler@spooner-web.de>
2018-04-20 13:47:12 +0200

    Change to stretch

--- Vagrantfile
+++ Vagrantfile
@@ -6,7 +6,7 @@ VAGRANTFILE_API_VERSION = "2"
 
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   # Every Vagrant virtual environment requires a box to build off of.
-  config.vm.box = "debian/jessie64"
+  config.vm.box = "debian/stretch64"
 
   config.vm.provider "virtualbox" do |v|
     v.memory = 2048 
@@ -18,4 +18,5 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   config.vm.network "private_network", ip: "192.168.150.5"
 
   config.vm.synced_folder "www/", "/var/www", :owner => 'vagrant', :group => 'www-data', :mount_options => ['dmode=775,fmode=775'], :create => true
+  config.vm.synced_folder "logs/", "/var/log/apache2", :owner => 'vagrant', :group => 'www-data', :mount_options => ['dmode=775,fmode=664'], :create => true
 end
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password password rootpass'
-sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password_again password rootpass'
+sudo debconf-set-selections <<< 'mariadb-server-10.1 mysql-server/root_password password rootpass'
+sudo debconf-set-selections <<< 'mariadb-server-10.1 mysql-server/root_password_again password rootpass'
 sudo apt-get update
-sudo apt-get -y install mysql-server-5.5 php5-mysql apache2 php5 php5-gd graphicsmagick wget curl git-core
+sudo apt-get -y install mysql-server php7.0-mysql apache2 php7.0 php7.0-gd php7.0-xml php7.0-json graphicsmagick wget curl git-core
 
 if [ ! -h /var/www ]; 
 then 
@@ -12,9 +12,9 @@ then
 
     sudo sed -i.bak '/DocumentRoot \/var\/www\/html/c DocumentRoot \/var\/www\/CmsBaseDistribution\/web' /etc/apache2/sites-available/000-default.conf
 
-    sudo sed -i.bak 's/max_execution_time = 30/max_execution_time = 240/' /etc/php5/apache2/php.ini
-    sudo sed -i.bak 's/;always_populate_raw_post_data = -1/always_populate_raw_post_data = -1/' /etc/php5/apache2/php.ini
-    sudo sed -i.bak 's/; max_input_vars = 1000/max_input_vars = 1500/' /etc/php5/apache2/php.ini
+    sudo sed -i.bak 's/max_execution_time = 30/max_execution_time = 240/' /etc/php/7.0/apache2/php.ini
+    sudo sed -i.bak 's/;always_populate_raw_post_data = -1/always_populate_raw_post_data = -1/' /etc/php/7.0/apache2/php.ini
+    sudo sed -i.bak 's/; max_input_vars = 1000/max_input_vars = 1500/' /etc/php/7.0/apache2/php.ini
 
     service apache2 restart
 fi
@@ -23,8 +23,6 @@ curl -Ss https://getcomposer.org/installer | php > /dev/null
 sudo mv composer.phar /usr/bin/composer
 chmod +x /usr/bin/composer
 cd /var/www/
-composer create-project typo3/cms-base-distribution CmsBaseDistribution
-touch /var/www/CmsBaseDistribution/web/FIRST_INSTALL
 
 
 
